ee8ed65f88de9e4e63fe5896c107c76dc6544569
Garret Denaro <67558539+Garret14@users.noreply.github.com>
2020-11-04 11:32:29 -0500

    Add files via upload

---
+++ Primality.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+        BigInteger b = new BigInteger(n);
+        String primecheck = "";
+        if(b.isProbablePrime(1)){
+            primecheck = "prime";
+        }
+        else{
+            primecheck = "not prime";
+        }
+        System.out.println(primecheck);
+        scanner.close();
+    }
+}
